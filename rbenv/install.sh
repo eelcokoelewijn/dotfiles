e4b03224fb49d2d72f393372e722695f039cafbe
Eelco Koelewijn <eelco@eelcokoelewijn.nl>
2020-10-17 13:35:49 +0200

    Optimize for Linux environment

--- rbenv/install.sh
+++ rbenv/install.sh
@@ -8,7 +8,15 @@
 if test ! $(which rbenv)
 then
   echo "  Installing rbenv for you."
-  brew install rbenv
+  # Install the correct homebrew for each OS type
+  if test "$(uname)" = "Darwin"
+  then
+    brew install rbenv  
+  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+  then
+    sh -c "$(curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer)"
+  fi  
+  
   echo "  Installing ruby 2.7.1."
   rbenv install "2.7.1"
   echo "  Make ruby 2.7.1 the global default."
