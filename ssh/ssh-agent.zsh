6bef39c378549bf3dc954b2de8fffc4d071fbf30
Eelco Koelewijn <eelco@eelcokoelewijn.nl>
2022-11-09 19:53:50 +0100

    fix(ssh): Typo in zsh load script

--- ssh/ssh-agent.zsh
+++ ssh/ssh-agent.zsh
@@ -1,7 +1,8 @@
 SKIP_SSH_AGENT="YES"
 if [ $SKIP_SSH_AGENT != "YES" ]; then
-# Launch ssh-agent
-if (( $+commands[ssh-agent] ))
-then
-    eval "$(ssh-agent -s)"
+    # Launch ssh-agent
+    if (( $+commands[ssh-agent] ))
+    then
+        eval "$(ssh-agent -s)"
+    fi
 fi
