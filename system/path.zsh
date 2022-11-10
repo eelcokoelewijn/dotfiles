0c2effd0410abc444756990af61c761d23dcb8c2
Eelco Koelewijn <eelco@eelcokoelewijn.nl>
2022-11-10 21:42:38 +0100

    chore(path): Define paths

--- system/path.zsh
+++ system/path.zsh
@@ -1,1 +1,7 @@
-export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$HOME/.nodenv/bin:$HOME/.local/bin:$PATH"
+HOME_BIN="$HOME/bin"
+USR_LOCAL_BIN="/usr/local/bin"
+USR_LOCAL_SBIN="/usr/local/sbin"
+DOTFILES_BIN="$DOTFILES/bin"
+NODENV="$HOME/.nodenv/bin"
+HOME_LOCAL_BIN="$HOME/.local/bin"
+export PATH="$HOME_BIN:$USR_LOCAL_BIN:$USR_LOCAL_SBIN:$DOTFILES_BIN:$NODENV:$HOME_LOCAL_BIN:$PATH"
