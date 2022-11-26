ea979bf1338a0166f4f8959f624f790313d0c6dd
Eelco Koelewijn <eelco@eelcokoelewijn.nl>
2022-11-26 11:19:24 +0100

    fix(ssh): Add keys only on macOS

--- ssh/ssh.zsh
+++ ssh/ssh.zsh
@@ -1,2 +1,11 @@
+#!/bin/sh
+#
+# SSH
+#
+# Add SSH keys to keychain.
+
+UNAME="$(uname -s)"
 # Add ssh keys
-ssh-add --apple-use-keychain
+if [ "$UNAME" = "Darwin" ]; then
+    ssh-add --apple-use-keychain
+fi
